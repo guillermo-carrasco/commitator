2f5dafba91027620fb8212bfe5e42ea0fb48a842
Guillermo Carrasco <guille.ch.88@gmail.com>
2014-04-10 16:51:31 +0200

    Added Fething data... modal

--- static/js/commitator.js
+++ static/js/commitator.js
@@ -2,7 +2,22 @@
 //  Fetching information methods  //
 ////////////////////////////////////
 
+var myApp;
+myApp = myApp || (function () {
+  var pleaseWaitDiv = $('<div class="modal fade bs-example-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><h3>Fetching data...</h3><div class="progress progress-striped active"><div class="progress-bar progress-bar-info" role="progressbar" aria-valuenow="10" aria-valuemin="0" aria-valuemax="100" style="width: 100%"><span class="sr-only"></span></div></div></div></div></div>');
+  return {
+    showPleaseWait: function() {
+      pleaseWaitDiv.modal();
+    },
+      hidePleaseWait: function () {
+        pleaseWaitDiv.modal('hide');
+      },
+
+  };
+})();
+
 function get_org_repos(org) {
+  myApp.showPleaseWait();
   $.getJSON('/api/org/commits?org=' + org, function(data) {
     //Prepare the data for the nvd3 plot
     chart_data = {'key': 'Total commits per repository', 'values': []};
@@ -39,23 +54,24 @@ function update_global_commits_per_repo(org) {
 //////////////////////
 
 function build_discrete_bar_chart(data) {
+  myApp.hidePleaseWait();
   nv.addGraph(function() {
     var chart = nv.models.discreteBarChart()
-      .x(function(d) { return d.label })
-      .y(function(d) { return d.value })
-      .staggerLabels(true)
-      .tooltips(true)
-      .showValues(true)
+    .x(function(d) { return d.label })
+    .y(function(d) { return d.value })
+    .staggerLabels(true)
+    .tooltips(true)
+    .showValues(true)
 
     d3.select('#main_chart svg')
-      .datum(data)
-      .transition().duration(500)
-      .call(chart)
-      ;
+    .datum(data)
+    .transition().duration(500)
+    .call(chart)
+    ;
 
-    nv.utils.windowResize(chart.update);
+  nv.utils.windowResize(chart.update);
 
-    return chart;
+  return chart;
   });
 }
 
